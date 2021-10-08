feaceb50a9e3841e95e8b28a7da99cd53cfc6009
Lexi <lexibu007@gmail.com>
2021-10-07 22:14:32 -0500

    Add JWT token interface and service

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,13 @@
+using API.Entities;
+using API.Interface;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
